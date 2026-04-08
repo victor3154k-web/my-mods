679c7ac79dc93210a8f857a37fee7483897b0c60
victor3154k-web <victor3154k@gmail.com>
2026-04-08 20:50:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('ola mundo  !');
+const btn = document.querySelector('.pontos');
+const sidebar = document.querySelector('.sidebar');
+
+btn.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+});
+
+document.addEventListener('click', (e) => {
+  if (!sidebar.contains(e.target) && !btn.contains(e.target)) {
+    sidebar.classList.remove('active');
+  }
+});
